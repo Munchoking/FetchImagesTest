3c3702b23236cb3ac10c50d1c2b884f417151a03
Munchoking <jen.kim29@gmail.com>
2023-02-28 16:41:43 -0500

    final class
    Declared a class as 'final'

--- ViewController.swift
+++ ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet weak var imageOne: UIImageView!
     @IBOutlet weak var imageTwo: UIImageView!
